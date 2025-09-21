59337e641dd4e93f61a33699cff48ff04c2e6a5d
nasim-raj-laskar <144606891+nasim-raj-laskar@users.noreply.github.com>
2025-09-21 15:33:38 +0530

    Dataset

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@ import sys
 import os
 import logging
 from src.logger import logging
+
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -18,7 +19,7 @@ class CustomException(Exception):
     
 # if __name__=="__main__":
 #     try:
-#         a=10/5
+#         a=10/0
 #     except Exception as e:
 #         logging.info("Divide by zero error")
 #         logging.info(CustomException(e, sys))
