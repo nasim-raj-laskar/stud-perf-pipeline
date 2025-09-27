138d27002586d157634d9706d9f5258506020b6a
nasim-raj-laskar <144606891+nasim-raj-laskar@users.noreply.github.com>
2025-09-27 14:11:28 +0530

    EC2 Deployment

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict_datapoint():
         return render_template('home.html', error="An error occurred during prediction. Please try again.")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=8080)
